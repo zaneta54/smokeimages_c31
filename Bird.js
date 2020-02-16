77559db475d2501cb014321216c208aa2de2f799
unknown <andrea_fuhrmann@hotmail.com>
2020-02-16 20:21:29 +0530

    created an array and added the smoke images

--- Bird.js
+++ Bird.js
@@ -2,6 +2,9 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
+
   }
 
   display() {
@@ -9,5 +12,14 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if (this.body.position.x>200){
+
+    
+    var position = [this.body.position.x,this.body.position.y];
+    this.trajectory.push(position);
+    }
+    for (var i = 0;i<this.trajectory.length;i++){
+      image(this.smokeimage,this.trajectory[i][0],this.trajectory[i][1]);
+    }
   }
 }
